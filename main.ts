d7a2f89b2f88e5eb73a4d37e8d8b8ebfe5e563de
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-28 06:45:28 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let numero1 = 0
+input.onButtonPressed(Button.A, function () {
+    numero1 = randint(1, 2)
+    if (numero1 == 1) {
+        basic.showIcon(IconNames.Diamond)
+    } else {
+        basic.showIcon(IconNames.SmallDiamond)
+    }
+})
